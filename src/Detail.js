3e7f07be6192ed9b25238d47a66de791c2559fe4
Keitar6 <104099101+Keitar6@users.noreply.github.com>
2022-06-22 20:52:24 +0200

    .

--- src/Detail.js
+++ src/Detail.js
@@ -5,36 +5,19 @@ import ErrorBoundares from "./ErrorBoundaries";
 import ThemeContext from "./ThemeContext";
 import Modal from "./Modal";
 class Details extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { loading: true };
-  // }
-
-  state = { loading: true, showModal: false }; // to samo co na górze tylko używając class properties.
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
-    //to jest praktycznie to samo co useEffect tyle że z klasami
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
-    );
+    const res = await fetch(`/petsApi/pets?id=${this.props.params.id}`);
     const json = await res.json();
 
-    this.setState(Object.assign({ loading: false }, json.pets[0])); //to samo co na dole (poniżej spread) tylko raz wywwołujemy setState co jest bardzo korzystne. Tutaj używamy object assign
-    // this.setState(Object.assign({ loading: false, ...json.pets[0] })); //to samo co na górze ale przy użyciu object spread operator
-
-    // this.setState({
-    //   loading:false
-    // })
-    // this.setState(json.pet[0])
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
   render() {
     if (this.state.loading) {
       return <h2>loading....</h2>;
     }
-
-    // throw new Error("Lma o you Crashed");
-
     const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
@@ -81,9 +64,4 @@ const WrappedDetails = () => {
     </ErrorBoundares>
   );
 };
-// const Details = () => {
-//     const {id} = useParams();
-//   return <h2>{id}</h2>;
-// };
-
 export default WrappedDetails;
